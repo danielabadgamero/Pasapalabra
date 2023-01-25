47ceaf51d290511bcf31c85c76b4e75e0194fb47
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-01-25 23:22:24 +0100

    Add definitions.

--- Definitions.h
+++ Definitions.h
@@ -37,28 +37,28 @@ std::vector<std::vector<std::u8string>> definitions[2]
 			u8"Persona que encabezó el último intento de reforma democrática."
 		},
 		{
-			u8"H",
-			u8"H def"
+			u8"Hubo",
+			u8"\"____ bastante oposición.\""
 		},
 		{
 			u8"Iglesias",
 			u8"(pl). Víctimas de las revueltas en Barcelona."
 		},
 		{
-			u8"J word",
-			u8"J def"
+			u8"Ejército",
+			u8"Contiene. Institución con poca intervención e importancia al principio de la Restauración."
 		},
 		{
-			u8"K word",
-			u8"K def"
+			u8"Kiwi",
+			u8"Fruta tropical con interior verde y núcleo blanco provisto de pepitas negras. Piel exterior con pelo y marrón."
 		},
 		{
-			u8"L word",
-			u8"L def"
+			u8"Lockout",
+			u8"Medida de la Patronal en reacción a la huelga de La Canadiense."
 		},
 		{
-			u8"M word",
-			u8"M def"
+			u8"Miseria",
+			u8"Estado extremo de pobreza. Presente en el campesinado de Andalucía y Extremadura."
 		},
 		{
 			u8"Nacionalismo",
@@ -73,28 +73,28 @@ std::vector<std::vector<std::u8string>> definitions[2]
 			u8"Práctica introducida por las tensiones entre la Patronal y los trabajadores. Consistía en la contratación de asesinos a sueldo para resolver las revueltas mediante la violencia."
 		},
 		{
-			u8"Q word",
-			u8"Q def"
+			u8"Comodín!",
+			u8"Comodín!"
 		},
 		{
 			u8"Ribera de, Primo",
 			u8"Dictador que instaura una dictadura en 1923."
 		},
 		{
-			u8"S word",
-			u8"S def"
+			u8"Sindicatos libres",
+			u8"Medida de la Patronal en reacción a la huelga de La Canadiense."
 		},
 		{
-			u8"T word",
-			u8"T def"
+			u8"Turno",
+			u8"Base de la Restauración"
 		},
 		{
-			u8"U word",
-			u8"U def"
+			u8"Comodín!",
+			u8"Comodín!"
 		},
 		{
-			u8"V word",
-			u8"V def"
+			u8"Ventajas",
+			u8"Brillaba por su ausencia en Marruecos."
 		},
 		{
 			u8"Comodín!",
@@ -128,52 +128,52 @@ std::vector<std::vector<std::u8string>> definitions[2]
 			u8"Sindicato representativo del anarquismo."
 		},
 		{
-			u8"Comodín",
-			u8"Comodín!"
+			u8"Débil",
+			u8"Estado del Estado."
 		},
 		{
-			u8"E word",
-			u8"E def"
+			u8"Expediente Picasso",
+			u8"Cuando un pintor va a la guerra."
 		},
 		{
 			u8"Fragmentación",
 			u8"Inicio o parte de la crisis del partido dinástico en el poder."
 		},
 		{
-			u8"G word",
-			u8"G def"
+			u8"Ley de Fugas",
+			u8"Contiene. Permitía a los policías disparar directamente a detenidos escapándose."
 		},
 		{
 			u8"Huelga",
 			u8"La CNT las decretaba."
 		},
 		{
-			u8"I word",
-			u8"I def"
+			u8"Incursiones",
+			u8"(pl). Se organizaron en Annual con el fin de controlar y someter a las tribus rifeñas."
 		},
 		{
-			u8"J word",
-			u8"J def"
+			u8"Rojo",
+			u8"Contiene. Temor a la revolución rusa."
 		},
 		{
-			u8"K word",
-			u8"K def"
+			u8"Kiwi",
+			u8"Ave autóctono de Nueva Zelanda."
 		},
 		{
 			u8"Líderes",
 			u8"(pl). Cánovas o Sagasta lo eran."
 		},
 		{
-			u8"M word",
-			u8"M def"
+			u8"Movilización",
+			u8"Efecto de la revolución Bolchevique en la clase obrera española."
 		},
 		{
-			u8"N word",
-			u8"N def"
+			u8"Anticatalanismo",
+			u8"Contiene. Crece a raíz del pistolerismo catalán."
 		},
 		{
-			u8"O word",
-			u8"O def"
+			u8"Organizaciones obreras",
+			u8"Obreros organizados."
 		},
 		{
 			u8"Partidos dinásticos",
@@ -188,24 +188,24 @@ std::vector<std::vector<std::u8string>> definitions[2]
 			u8"Uno de los grupos de la oposición al sistema."
 		},
 		{
-			u8"S word",
-			u8"S def"
+			u8"Práxedes Mariano Mateo-Sagasta y Escolar",
+			u8"Contiene. Nombre COMPLETO del líder del partido liberal durante un período."
 		},
 		{
-			u8"T word",
-			u8"T def"
-		},
-		{
-			u8"U word",
-			u8"U def"
-		},
-		{
-			u8"V word",
-			u8"V def"
+			u8"FNTT",
+			u8"Contiene. Creada por la UGT en Andalucía y Extremadura."
 		},
 		{
 			u8"Comodín!",
 			u8"Comodín!"
+		},
+		{
+			u8"Comodín!",
+			u8"Comodín!"
+		},
+		{
+			u8"1898",
+			u8"Contiene. Año en el que ocurrió un desastre en Cuba."
 		},
 		{
 			u8"Xilófono",
